1568ea50d8c18c1fba098c5f88d457c3379fb5f2
Anupam Singh <eranupamsingh1008@gmail.com>
2025-06-29 16:04:20 +0530

    ElevatorRides.java file added

--- Dynamic_Programming/ElevatorRides.java
+++ Dynamic_Programming/ElevatorRides.java
@@ -2,71 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class ElevatorRides {
-    static class Subset {
-        int sum, mask;
-        Subset(int sum, int mask) {
-            this.sum = sum;
-            this.mask = mask;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter(System.out);
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
         int n = Integer.parseInt(st.nextToken());
         int x = Integer.parseInt(st.nextToken());
         
-        int[] weights = new int[n];
         st = new StringTokenizer(br.readLine());
+        int[] w = new int[n];
         for (int i = 0; i < n; i++) {
-            weights[i] = Integer.parseInt(st.nextToken());
+            w[i] = Integer.parseInt(st.nextToken());
         }
         
-        int half = n / 2;
-        List<Subset> firstHalf = new ArrayList<>();
-        
-        // Generate all subsets of first half
-        for (int mask = 0; mask < (1 << half); mask++) {
-            int sum = 0;
-            for (int i = 0; i < half; i++) {
-                if ((mask & (1 << i)) != 0) {
-                    sum += weights[i];
-                }
-            }
-            firstHalf.add(new Subset(sum, mask));
+        // DP state: dp[mask] = {min_rides, min_last_weight}
+        int[][] dp = new int[1 << n][2];
+        for (int[] row : dp) {
+            Arrays.fill(row, Integer.MAX_VALUE);
         }
+        dp[0][0] = 1;
+        dp[0][1] = 0;
         
-        // Generate all subsets of second half
-        int secondHalf = n - half;
-        int minRides = Integer.MAX_VALUE;
-        
-        for (int mask = 0; mask < (1 << secondHalf); mask++) {
-            int sum = 0;
-            for (int i = 0; i < secondHalf; i++) {
-                if ((mask & (1 << i)) != 0) {
-                    sum += weights[half + i];
-                }
+        for (int mask = 0; mask < (1 << n); mask++) {
+            int rides = dp[mask][0];
+            int currentWeight = dp[mask][1];
+            if (rides == Integer.MAX_VALUE) {
+                continue;
             }
-            
-            // Find best complement in first half
-            if (sum <= x) {
-                int remaining = x - sum;
-                int best = 0;
-                
-                for (Subset s : firstHalf) {
-                    if (s.sum <= remaining) {
-                        best = Math.max(best, s.sum);
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) == 0) {
+                    int newMask = mask | (1 << i);
+                    int newRides = rides;
+                    int newWeight = currentWeight + w[i];
+                    if (newWeight > x) {
+                        newRides++;
+                        newWeight = w[i];
+                    }
+                    if (newRides < dp[newMask][0] || (newRides == dp[newMask][0] && newWeight < dp[newMask][1])) {
+                        dp[newMask][0] = newRides;
+                        dp[newMask][1] = newWeight;
                     }
                 }
-                
-                int rides = Integer.bitCount(mask) + Integer.bitCount(best);
-                minRides = Math.min(minRides, rides);
             }
         }
         
-        pw.println(minRides);
-        pw.flush();
+        System.out.println(dp[(1 << n) - 1][0]);
     }
 }
