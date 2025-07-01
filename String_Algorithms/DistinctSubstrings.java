3728e8c0c982c02c63e97a16f0b8bb6536422cf1
Anupam Singh <eranupamsingh1008@gmail.com>
2025-07-01 18:08:59 +0530

    DistinctSubstrings.java file added

---
+++ String_Algorithms/DistinctSubstrings.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class DistinctSubstrings {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        Set<String> substrings = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                substrings.add(s.substring(i, j));
+            }
+        }
+        System.out.println(substrings.size());
+    }
+}
